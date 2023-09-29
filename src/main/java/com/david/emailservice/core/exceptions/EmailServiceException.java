3d4f23f88d2a9518239f3a0b781ff779368f6ad7
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-09-29 10:35:06 -0300

    Exceptions

---
+++ src/main/java/com/david/emailservice/core/exceptions/EmailServiceException.java
@@ -0,0 +1,14 @@
+package com.david.emailservice.core.exceptions;
+
+public class EmailServiceException extends RuntimeException {
+
+    public EmailServiceException(String message) {
+
+        super(message);
+    }
+
+    public EmailServiceException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+}
